fb02435abf98f92ed633bdb70d4a17744a37e176
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2020-06-11 16:37:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords='slugify slug transliteration russian german unicode translation flexible',
 )
